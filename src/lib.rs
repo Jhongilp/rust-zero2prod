7124d6d2fa9e5c114d44d0b5e95f3a1af2b4fb62
Jonathan Gil <jhongilp@gmail.com>
2023-03-03 17:24:00 -0500

    implement health_check test

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,23 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
-  let name = req.match_info().get("name").unwrap_or("World");
-  format!("Hello {}!", &name)
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
 async fn health_check() -> impl Responder {
-  HttpResponse::Ok()
+    HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-  HttpServer::new(|| {
-      App::new()
-          .route("/", web::get().to(greet))
-          .route("/{name}", web::get().to(greet))
-          .route("/health_check", web::get().to(health_check))
-  })
-  .bind("127.0.0.1:8000")?
-  .run()
-  .await
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run();
+    // .await
+    Ok(server)
 }
